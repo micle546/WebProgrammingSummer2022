b9607fbdc7169f75b910630cdd24d9266d393c43
micle546 <micle546@gmail.com>
2022-06-14 21:29:33 -0400

    updates to templates

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from this import s
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
@@ -7,4 +8,11 @@ app = Flask(__name__)
 def get_index():
     return "<p>test22 (╯°□°）╯︵ ǝƃɐdqǝʍ</p>"
 
+@app.route("/hello")
+def get_hello():
+    return render_template('hello.html', name='Mike')
+
+@app.route("/santa")
+def get_santa():
+    return render_template('hello.html', name='Santa')
 
